738b2fbeb8cfe04b9ebedbe4db21c5ed9de9b410
higorasilverio <higorasilverio@outlook.com>
2020-04-10 16:55:47 -0300

    Reeds numbers and verify the interval - Final

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Reeds numbers and verify the interval ***");
+		System.out.println();
+		
+		int cont_in = 0, cont_out = 0;
+		System.out.print("How many numbers we will verify? ");
+		int quantity = scanner.nextInt();
+		System.out.println("Enter the numbers to verify if they are between 10 and 20: ");
+		for(int i = 0; i < quantity; i++) {
+			int number = scanner.nextInt();
+			if (number >= 10 && number <= 20) cont_in++;
+			else cont_out++;
+		}
+		
+		System.out.println();
+		System.out.printf("Number(s) in: %d\nNumber(s) out: %d\n", cont_in, cont_out);
+		
+		scanner.close();
+		
+
+	}
+
+}
